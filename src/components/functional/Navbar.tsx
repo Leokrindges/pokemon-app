91d2316f70d9e99730a0502a932c5f75433a0ee8
leonardo Krindges <krindgesleonardo3@gmail.com>
2024-08-17 11:32:48 -0300

    adicionado navegação entre as paginas home e pokedex

--- src/components/functional/Navbar.tsx
+++ src/components/functional/Navbar.tsx
@@ -2,29 +2,25 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
 import Button from "@mui/material/Button";
+import { useNavigate } from "react-router-dom";
 
 export default function Navbar() {
+  const navigate = useNavigate();
 
   return (
     <Box sx={{ flexGrow: 3 }}>
       <AppBar position="fixed">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
+          <Typography
+            onClick={() => navigate("/pokemon")}
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, fontWeight: "bold", cursor: "pointer" }}
           >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1, fontWeight:'bold'}}>
             POKEMON
           </Typography>
-          <Button color="inherit">
+          <Button onClick={() => navigate("/pokedex")} color="inherit">
             Pokedex
           </Button>
         </Toolbar>
