437492ec5b562cdf5785b696f023982bbae65ea2
leonardo Krindges <krindgesleonardo3@gmail.com>
2024-08-19 21:16:12 -0300

    Adicionado redux-persist

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,21 +1,35 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { pokemonAdapter, pokemonSlice } from "./modules/pokemons/pokemonsSlice";
-import { pokedexAdapter, pokedexSlice } from "./modules/pokedex/pokedex.slice";
+import { pokemonSlice } from "./modules/pokemons/pokemonsSlice";
+import { FLUSH, PAUSE, PERSIST, persistReducer, persistStore, PURGE, REGISTER, REHYDRATE } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { rootReducer } from "./modules/rootReducer";
+
+const persistedReducer = persistReducer(
+  {
+    key: "app-store",
+    storage: storage, // defaults to localStorage for web
+    whitelist: ["pokedex"],
+  },
+  rootReducer
+);
+
+// export const store = configureStore({
+//   reducer: { pokemons: pokemonSlice.reducer, pokedex: pokedexSlice.reducer },
+// });
 
 export const store = configureStore({
-  reducer: { pokemons: pokemonSlice.reducer, pokedex: pokedexSlice.reducer },
+  reducer: { pokemons: pokemonSlice.reducer, persistedReducer },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+// configuração da persistencia dos dados no localStorage
+export const persistor = persistStore(store);
 
 export type GlobalState = ReturnType<typeof store.getState>;
 
 export type AppDispatch = typeof store.dispatch;
-const pokemonSelector = pokemonAdapter.getSelectors<GlobalState>(
-  (state) => state.pokemons
-);
-const pokedexSelector = pokedexAdapter.getSelectors<GlobalState>(
-  (state) => state.pokedex
-);
-
-export const allPokemons = pokemonSelector.selectAll(store.getState());
-
-export const allPokemonPokedex = pokedexSelector.selectAll(store.getState());
--- src/store/rootReducer.ts
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "@reduxjs/toolkit";
-import { pokemonReducer } from "./modules/pokemons/pokemonsSlice";
-
-export const rootReducer = combineReducers({
-  pokemons: pokemonReducer,
-});
